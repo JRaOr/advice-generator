1268ecfb8cab923507964b00a2e0ab2e0a582d85
JRaOr <laocura@gmail.com>
2022-02-19 22:34:36 -0600

    first commit

---
+++ README-template.md
@@ -0,0 +1,31 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Get a advice every time
+- Click the dice and get a new advice
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- [React](https://reactjs.org/) - JS library
+
+## Author
+
+- Frontend Mentor - [@JRaOr](https://www.frontendmentor.io/profile/JRaOr)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [advice, setAdvice] = useState({id: null, advice: ''})
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        getAdvice()
+    },[])
+
+    async function getAdvice(){
+        let response  = await axios.get('https://api.adviceslip.com/advice')
+        console.log(response)
+        if(response.status === 200)
+        setAdvice(response.data.slip)
+        setLoading(false)
+    }
+
+    return(
+        <div className="container">
+            <div className="card">
+                <h1 className="title">Advice # {advice.id}</h1>
+                <p className="advice">"{advice.advice}"</p>
+                <img className="divider" src='./images/pattern-divider-desktop.svg'/>
+                <div onClick={()=>{
+                    setTimeout(()=>{
+                        getAdvice()
+                    }, 2000)
+                    setLoading(true)
+                }} className={`dice ${loading && 'dice-animation'}`}>
+                    <img src='./images/icon-dice.svg'/>
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App;
